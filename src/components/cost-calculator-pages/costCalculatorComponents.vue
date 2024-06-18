68e87a7d949186e738ca60f0360d527559a6897c
Ibe Augustine <aibe64@yahoo.com>
2024-06-18 23:48:43 +0100

    Current Updates

--- src/components/cost-calculator-pages/costCalculatorComponents.vue
+++ src/components/cost-calculator-pages/costCalculatorComponents.vue
@@ -2351,7 +2351,7 @@ import Imagegiver3 from '@/assets/images/Care_Service/Frame7.png'
           // border: 2px solid red;
           margin-bottom: 2.5rem;
           font-weight: 400;
-          font-size: 30px;
+          font-size: 30px !important;
           line-height: 30px;
           padding-left: 0px;
           text-align: center;
@@ -2365,7 +2365,7 @@ import Imagegiver3 from '@/assets/images/Care_Service/Frame7.png'
         .intro-subtitle {
           // border: 2px solid red;
           font-weight: 700;
-          font-size: 35px;
+          font-size: 35px !important;
           line-height: 50px;
           letter-spacing: 0em;
           color: #fff;
