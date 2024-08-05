d086cf236239329e95f4c4029670882d57efdd1c
Ibe Augustine <aibe64@yahoo.com>
2024-08-05 12:11:40 +0100

    New Current Updates

--- src/components/home-page-containers/banner/HomePagebanner.vue
+++ src/components/home-page-containers/banner/HomePagebanner.vue
@@ -428,7 +428,7 @@ const imageUrl = [
                 LOVENEST SPOTLIGHT
               </h2>
               <p>
-                Hi. My name is Kayand I'm the Assistant Director of
+                Hi. My name is Kay and I'm the Assistant Director of
                 LoveNest Care. Every day, people entrust us to care for
                 their loved ones, and that is a responsibility I take very
                 seriously. In my role, I am responsible for training and
